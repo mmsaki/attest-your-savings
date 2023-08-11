eebbd1f0163f0ef3d44a6f7ec0d1269717bf096e
Meek Msaki <meek10x@gmail.com>
2023-08-11 05:39:15 -0500

    feat: install tailwind and wagmi

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,36 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				'4xl': { max: '1719px' },
+				'2xl': { max: '1419px' },
+				xl: { max: '1259px' },
+				xls: { max: '1179px' },
+				lg: { max: '1023px' },
+				md: { max: '767px' },
+				sm: { max: '480px' },
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+				4: '4',
+				5: '5',
+			},
+			borderRadius: {
+				1: '0.0635rem',
+			},
+			colors: {
+				background: '#f4f4f0',
+				n: {
+					1: '#000000',
+					2: '#161616',
+					3: '#5f646d',
+					4: '#e7e8e9',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
