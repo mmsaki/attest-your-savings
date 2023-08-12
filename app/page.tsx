89af9ade48068bf781cc154826bdbb97e1168b23
Meek Msaki <meek10x@gmail.com>
2023-08-11 21:45:19 -0500

    chore: added spense

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
-import { useAccount } from 'wagmi';
+import { useAccount, useNetwork } from 'wagmi';
 import UseConnect from '@/app/hooks/UseConnect';
 import UseSwitchNetwork from '@/app/hooks/UseSwitchNetwork';
 import UseBalance from '@/app/hooks/UseBalance';
 import UseToken from '@/app/hooks/UseToken';
 import UseBlockNumber from '@/app/hooks/UseBlockNumber';
+import { Suspense } from 'react';
 
 export default function Home() {
 	const { address, isConnected } = useAccount();
+	const { chain } = useNetwork();
 	return (
 		<main>
 			<div className='flex flex-col'>
@@ -16,7 +18,7 @@ export default function Home() {
 			<div className='flex flex-col'>
 				<UseSwitchNetwork />
 			</div>
-			{isConnected && (
+			{isConnected && chain?.network === 'sepolia' && (
 				<>
 					<div className=''>
 						<UseBalance address={address} />
@@ -26,8 +28,12 @@ export default function Home() {
 						/>
 					</div>
 					<h1 className='text-2xl'>Cross Chain Saving Account!</h1>
-					<UseToken address={'0x3393C3b8DD9e44657358e6fE092D9a3110181E78'} />
-					<UseBlockNumber />
+					<Suspense fallback={<p>Loading saving details...</p>}>
+						<UseToken address={'0x3393C3b8DD9e44657358e6fE092D9a3110181E78'} />
+					</Suspense>
+					<Suspense fallback={<p>Loading block number...</p>}>
+						<UseBlockNumber />
+					</Suspense>
 				</>
 			)}
 		</main>
